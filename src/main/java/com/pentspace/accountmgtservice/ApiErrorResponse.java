70dc040b7ffde671055b19768717966427070d61
amigoscode <salamikehinde345@gmail.com>
2023-05-26 14:09:34 +0100

    fix

--- src/main/java/com/pentspace/accountmgtservice/ApiErrorResponse.java
+++ src/main/java/com/pentspace/accountmgtservice/ApiErrorResponse.java
@@ -1,21 +1,13 @@
 package com.pentspace.accountmgtservice;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-
-import java.time.LocalDateTime;
 
 @Data
 @RequiredArgsConstructor
 public class ApiErrorResponse {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "yyyy-MM-dd hh:mm:ss")
-    private final LocalDateTime localDateTime;
-    private final HttpStatus httpStatus;
     private final String message;
-    private final String details;
 
 
 }
--- src/main/java/com/pentspace/accountmgtservice/exceptions/CustomizedResponseEntityHandler.java
+++ src/main/java/com/pentspace/accountmgtservice/exceptions/CustomizedResponseEntityHandler.java
@@ -22,29 +22,22 @@ import java.util.List;
 public class CustomizedResponseEntityHandler extends ResponseEntityExceptionHandler {
 
         @ExceptionHandler(Exception.class)
-        public final ResponseEntity<ApiErrorResponse> handleAllExceptions(Exception exception,
-                                                                          WebRequest request) {
-            ApiErrorResponse apiErrorResponse = new ApiErrorResponse(LocalDateTime.now(),HttpStatus.INTERNAL_SERVER_ERROR,
-                    exception.getMessage(),request.getDescription(false));
+        public final ResponseEntity<ApiErrorResponse> handleAllExceptions(Exception exception) {
+            ApiErrorResponse apiErrorResponse = new ApiErrorResponse(exception.getMessage());
 
             return new ResponseEntity<>(apiErrorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
     @ExceptionHandler(MessagingException.class)
-    public final ResponseEntity<ApiErrorResponse> handleAllExceptions(MessagingException messagingException,
-                                                                      WebRequest request) {
-        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(LocalDateTime.now(),HttpStatus.INTERNAL_SERVER_ERROR,
-                messagingException.getMessage(),request.getDescription(false));
+    public final ResponseEntity<ApiErrorResponse> handleAllExceptions(MessagingException messagingException) {
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(messagingException.getMessage());
 
-        return new ResponseEntity<>(apiErrorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
         @ExceptionHandler(AuthorizationException.class)
-        public final ResponseEntity<ApiErrorResponse> handleUserNotFoundException(AuthorizationException authorizationException,
-                                                                                  WebRequest request) {
-            ApiErrorResponse apiErrorResponse = new ApiErrorResponse(LocalDateTime.now(),HttpStatus.UNAUTHORIZED,
-                    authorizationException.getMessage(),
-                    request.getDescription(false));
+        public final ResponseEntity<ApiErrorResponse> handleUserNotFoundException(AuthorizationException authorizationException) {
+            ApiErrorResponse apiErrorResponse = new ApiErrorResponse(authorizationException.getMessage());
 
             return new ResponseEntity<>(apiErrorResponse, HttpStatus.UNAUTHORIZED);
         }
@@ -52,9 +45,7 @@ public class CustomizedResponseEntityHandler extends ResponseEntityExceptionHand
     @ExceptionHandler(GeneralServiceException.class)
     public final ResponseEntity<ApiErrorResponse> handleUserNotFoundException(GeneralServiceException generalServiceException,
                                                                               WebRequest request) {
-        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(LocalDateTime.now(),HttpStatus.BAD_REQUEST,
-               generalServiceException.getMessage(),
-                request.getDescription(false));
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(generalServiceException.getMessage());
 
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
@@ -62,11 +53,8 @@ public class CustomizedResponseEntityHandler extends ResponseEntityExceptionHand
 
 
     @ExceptionHandler(AccountCreationException.class)
-    public final ResponseEntity<ApiErrorResponse> handleUserNotFoundException(AccountCreationException accountCreationException,
-                                                                              WebRequest request) {
-        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(LocalDateTime.now(),HttpStatus.BAD_REQUEST,
-                accountCreationException.getMessage(),
-                request.getDescription(false));
+    public final ResponseEntity<ApiErrorResponse> handleUserNotFoundException(AccountCreationException accountCreationException) {
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(accountCreationException.getMessage());
 
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.BAD_REQUEST);
     }
@@ -74,25 +62,19 @@ public class CustomizedResponseEntityHandler extends ResponseEntityExceptionHand
 
 
     @ExceptionHandler(UsernameNotFoundException.class)
-    public final ResponseEntity<ApiErrorResponse> handleUserNotFoundException(UsernameNotFoundException usernameNotFoundException,
-                                                                              WebRequest request) {
-        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(LocalDateTime.now(),HttpStatus.NOT_FOUND,
-                usernameNotFoundException.getMessage(),
-                request.getDescription(false));
+    public final ResponseEntity<ApiErrorResponse> handleUserNotFoundException(UsernameNotFoundException usernameNotFoundException
+                                                                              ) {
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(usernameNotFoundException.getMessage());
 
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(IncorrectPasswordException.class)
-    public final ResponseEntity<ApiErrorResponse> handleUserNotFoundException(IncorrectPasswordException incorrectPasswordException,
-                                                                              WebRequest request) {
-        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(LocalDateTime.now(),HttpStatus.NOT_FOUND,
-                incorrectPasswordException.getMessage(),
-                request.getDescription(false));
+    public final ResponseEntity<ApiErrorResponse> handleUserNotFoundException(IncorrectPasswordException incorrectPasswordException) {
+        ApiErrorResponse apiErrorResponse = new ApiErrorResponse(incorrectPasswordException.getMessage());
 
         return new ResponseEntity<>(apiErrorResponse, HttpStatus.NOT_FOUND);
     }
-
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -100,7 +82,7 @@ public class CustomizedResponseEntityHandler extends ResponseEntityExceptionHand
         for(ObjectError error : ex.getBindingResult().getAllErrors()) {
             details.add(error.getDefaultMessage());
         }
-        ApiErrorResponse error = new ApiErrorResponse(LocalDateTime.now(),HttpStatus.BAD_REQUEST,"Validation Failed" , details.toString());
+        ApiErrorResponse error = new ApiErrorResponse("Validation Failed" );
         return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
     }
 
